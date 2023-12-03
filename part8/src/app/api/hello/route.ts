e7c3fb0a1ab4b0d552931769a4b5fbb739af6c77
iql981 <iql981@my.utsa.edu>
2023-12-03 13:17:54 -0600

    part9 completed
    Learned how to implement middleware packages such as CORS and limiter to further streamline requests to NextJS API calls.

---
+++ part8/src/app/api/hello/route.ts
@@ -0,0 +1,25 @@
+import { NextDataPathnameNormalizer } from "next/dist/server/future/normalizers/request/next-data";
+import { limiter } from "../config/limiter";
+import { NextResponse } from "next/server";
+
+export async function GET(request:Request) {
+
+    const origin = request.headers.get('origin')
+
+    const remaining = await limiter.removeTokens(1)
+    console.log('remaining: ', remaining)
+
+    if (remaining < 0)
+    {
+        return new NextResponse(null, {
+            status: 429,
+            statusText: "Too many requests",
+            headers: {
+                'Access-Control-Allow-Origin': origin || '*',
+                'Content-Type': 'text/plain'
+            }
+        })
+    }
+
+    return new Response('')
+}
